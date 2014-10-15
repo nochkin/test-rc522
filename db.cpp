39b915b5159e8d412e5ec170ebc91807f4d10a3b
root <root@Raspyfi>
2014-10-15 02:25:44 +0100

    DB: added update/select methods

--- db.cpp
+++ db.cpp
@@ -59,6 +59,7 @@ int DB::add_new(std::string tag)
 	int rc;
 	static char const *args_s[] = {
 		"",
+		tag.c_str(),
 		tag.c_str()
 	};
 	unsigned long args_i[] = {
@@ -67,26 +68,36 @@ int DB::add_new(std::string tag)
 	};
 	uint8_t args[] = {
 		SQLITE_INTEGER,
+		SQLITE_TEXT,
 		SQLITE_TEXT
 	};
 
-	rc = run_sql(DB_INSERT_TAG, args_i, args_s, args, 2);
+	rc = sql_update(DB_INSERT_TAG, args_i, args_s, args, 3);
 	if (rc == SQLITE_OK) {
-		rc = run_sql(DB_UPDATE_TIME, args_i, args_s, args, 2);
-		if (rc == SQLITE_OK) {
-			printf("time updated\n");
-		} else {
-			printf("update_time: %s\n", get_error().c_str());
+		rc = sql_update(DB_UPDATE_TIME, args_i, args_s, args, 2);
+		if (rc != SQLITE_OK) {
+			printf("DB: error update_time: %s\n", get_error().c_str());
 		}
 	} else {
-		printf("insert_tag: %s\n", get_error().c_str());
+		printf("DB: error insert_tag: %s\n", get_error().c_str());
 	}
 
 	return rc;
 }
 
-std::string DB::get_playfile(std::string tag)
+int DB::update_playfile(std::string tag, std::string playfile)
 {
+	return this->update_text(DB_UPDATE_FILE, tag, playfile);
+}
+
+int DB::update_tagname(std::string tag, std::string tagname)
+{
+	return this->update_text(DB_UPDATE_NAME, tag, tagname);
+}
+
+tag_t DB::get_taginfo(std::string tag)
+{
+	tag_t tag_info;
 	int rc;
 
 	static char const *args_s[] = {
@@ -99,41 +110,78 @@ std::string DB::get_playfile(std::string tag)
 		SQLITE_TEXT
 	};
 
-	rc = run_sql(DB_SELECT_FILE, args_i, args_s, args, 1);
-	if (rc == SQLITE_OK) {
+	rc = this->sql_run(DB_SELECT_TAG, args_i, args_s, args, 1);
+	while (1) {
+		rc = sqlite3_step(this->stmt);
+		if (rc == SQLITE_DONE) break;
+		else if (rc == SQLITE_ROW) {
+			tag_info.tag = tag;
+			tag_info.tagname = reinterpret_cast<const char*>(sqlite3_column_text(this->stmt, 0));
+			tag_info.mytime = sqlite3_column_int(this->stmt, 1);
+			tag_info.playfile = reinterpret_cast<const char*>(sqlite3_column_text(this->stmt, 2));
+		} else {
+			printf("step(%i): %s\n", rc, get_error().c_str());
+		}
 	}
+	rc = sqlite3_finalize(this->stmt);
 
-	return "";
+	return tag_info;
 }
 
-int DB::run_sql(std::string sql, unsigned long args_i[], const char *args_s[], uint8_t args[], uint8_t argn)
+int DB::update_text(std::string sql, std::string tag, std::string textfield)
 {
 	int rc;
-	sqlite3_stmt *stmt;
+	static char const *args_s[] = {
+		textfield.c_str(),
+		tag.c_str()
+	};
+	unsigned long args_i[] = {
+		0
+	};
+	uint8_t args[] = {
+		SQLITE_TEXT,
+		SQLITE_TEXT
+	};
 
-	rc = sqlite3_prepare_v2(this->mydb, sql.c_str(), strlen(sql.c_str()), &stmt, NULL);
+	rc = sql_update(sql.c_str(), args_i, args_s, args, 2);
+	if (rc != SQLITE_OK) {
+		printf("DB: error update_text: %s\n", get_error().c_str());
+	}
+
+	return rc;
+}
+
+int DB::sql_update(std::string sql, unsigned long args_i[], const char *args_s[], uint8_t args[], uint8_t argn)
+{
+	int rc;
+   
+	rc = this->sql_run(sql, args_i, args_s, args, argn);
+	if (rc == SQLITE_OK) {
+		rc = sqlite3_step(this->stmt);
+		if (rc == SQLITE_DONE) {
+			rc = sqlite3_finalize(this->stmt);
+		}
+	}
+
+	return rc;
+}
+
+int DB::sql_run(std::string sql, unsigned long args_i[], const char *args_s[], uint8_t args[], uint8_t argn)
+{
+	int rc;
+
+	rc = sqlite3_prepare_v2(this->mydb, sql.c_str(), strlen(sql.c_str()), &this->stmt, NULL);
 	if (rc == SQLITE_OK) {
 		for (uint8_t ii=0; ii<argn; ii++) {
 			switch (args[ii]) {
 				case SQLITE_TEXT:
-					sqlite3_bind_text(stmt, ii+1, args_s[ii], strlen(args_s[ii]), 0);
+					sqlite3_bind_text(this->stmt, ii+1, args_s[ii], strlen(args_s[ii]), 0);
 					break;
 				case SQLITE_INTEGER:
-					sqlite3_bind_int(stmt, ii+1, args_i[ii]);
+					sqlite3_bind_int(this->stmt, ii+1, args_i[ii]);
 					break;
 			}
 		}
-		while (1) {
-			rc = sqlite3_step(stmt);
-			// printf("step(%i): %s\n", rc, get_error().c_str());
-			if (rc == SQLITE_DONE) break;
-			if (rc == SQLITE_ROW) {
-				std::string column_text(reinterpret_cast<const char*>(sqlite3_column_text(stmt, 0)));
-				printf("column: %s\n", column_text.c_str());
-			}
-		}
-		rc = sqlite3_finalize(stmt);
-		// printf("finalize(%i): %s\n", rc, get_error().c_str());
 	}
 
 	return rc;
--- db.h
+++ db.h
@@ -8,11 +8,19 @@
 #include <string.h>
 #include <sys/time.h>
 
-#define DB_CREATE_TAGS	"create table if not exists rfid_tags(tag text primary key not null,mytime int not null,playfile text default '')"
-#define DB_INSERT_TAG	"insert or ignore into rfid_tags (mytime,tag) values (?,?)"
+#define DB_CREATE_TAGS	"create table if not exists rfid_tags(tag text primary key not null,tagname text not null unique,mytime int not null,playfile text default '')"
+#define DB_INSERT_TAG	"insert or ignore into rfid_tags (mytime,tag,tagname) values (?,?,?)"
 #define DB_UPDATE_TIME	"update rfid_tags set mytime=? where tag=?"
 #define DB_UPDATE_FILE	"update rfid_tags set playfile=? where tag=?"
-#define DB_SELECT_FILE	"select playfile from rfid_tags where tag=?"
+#define DB_UPDATE_NAME	"update rfid_tags set tagname=? where tag=?"
+#define DB_SELECT_TAG	"select tagname,mytime,playfile from rfid_tags where tag=?"
+
+struct tag_t {
+	std::string tag;
+	std::string tagname;
+	unsigned long mytime;
+	std::string playfile;
+};
 
 class DB
 {
@@ -23,13 +31,18 @@ class DB
 		int close();
 		std::string get_error();
 		int add_new(std::string tag);
-		std::string get_playfile(std::string tag);
+		int update_playfile(std::string tag, std::string playfile);
+		int update_tagname(std::string tag, std::string tagname);
+		tag_t get_taginfo(std::string tag);
 	private:
 		sqlite3 *mydb;
+		sqlite3_stmt *stmt;
 		std::string my_filename;
 
 		int create();
-		int run_sql(std::string sql, unsigned long args_i[], const char *args_s[], uint8_t args[], uint8_t argn);
+		int update_text(std::string sql, std::string tag, std::string textfield);
+		int sql_update(std::string sql, unsigned long args_i[], const char *args_s[], uint8_t args[], uint8_t argn);
+		int sql_run(std::string sql, unsigned long args_i[], const char *args_s[], uint8_t args[], uint8_t argn);
 };
 
 #endif
