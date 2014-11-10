7d5be019a22d5e171999f94f73525df3d7801f61
nochkin <nochkin@durdom.com>
2014-11-10 13:54:51 -0800

    Log tags to syslog

--- controller.cpp
+++ controller.cpp
@@ -24,19 +24,19 @@ int Controller::setup_mpclient() {
 }
 
 void Controller::main_loop() {
-	uint8_t status;
-
 	while(1)
 	{
-		status = this->rfreader.read_tag();
+		uint8_t status = this->rfreader.read_tag();
 		// status = TAG_OK;
 		if (status == TAG_OK) {
 			std::string mytag = this->rfreader.get_tag_str();
 			printf("tag: %s\n", mytag.c_str());
+			syslog(LOG_DAEMON|LOG_INFO, "Tag: %s\n", mytag.c_str());
 			this->tagdb.add_new(mytag);
 			tag_t mytag_info = this->tagdb.get_taginfo(mytag);
 			if (!mytag_info.playfile.empty()) {
 				printf("playfile: %s\n", mytag_info.playfile.c_str());
+				syslog(LOG_DAEMON|LOG_INFO, "playfile: %s\n", mytag_info.playfile.c_str());
 				this->mpclient.add_and_play(mytag_info.playfile);
 			}
 		}
